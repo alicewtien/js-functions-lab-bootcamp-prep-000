1b3368572537ca6f261801e5d97cda60626857d2
Ali T <alicewtien@gmail.com>
2018-06-24 02:12:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,10 @@ function happyHolidaysTo(name) {
   console.log(`Happy Holidays, ${name}`)
 }
 
-function happyHolidayTo(holiday, name)
+function happyHolidayTo(holiday, name) {
+  console.log(`Happy ${holiday}, ${name}!`)
+}
+
+function holidayCountDown(days, holiday) {
+  console.log(`It's ${days} days until ${holiday}!`)
+}
